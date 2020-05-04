af4abe9df9c4b4ec8d59b1c6c6d603c513cc46b9
nassim530u <nassimboulachfar@gmail.com>
2020-05-04 17:30:06 +0100

    add migrate

--- models.py
+++ models.py
@@ -30,6 +30,7 @@ def setup_db(app, database_uri=database_uri):
     app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
+    migrate = Migrate(app, db)
     db.init_app(app)
 
 # Movie
